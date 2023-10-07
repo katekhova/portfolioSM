33220ebefc0a7e2c8c34ca8e53f68ce1094eac2b
katekhova <kate.khova@gmail.com>
2023-10-07 15:04:16 +0200

    changed var to let in js code

--- deploy/scripts/main.js
+++ deploy/scripts/main.js
@@ -1,5 +1,5 @@
 const amountOfPhotos = 9;
-var currentPhoto = 0;
+let currentPhoto = 0;
 addAllPhotosToHtml(amountOfPhotos);
 addAllDotsToHtml(amountOfPhotos);
 addDotEventListener(amountOfPhotos);
@@ -14,7 +14,7 @@ document.getElementsByClassName("menu")[0].addEventListener("click", openMenu);
 
 function addAllPhotosToHtml(amount) {
   photosConteiner = document.getElementsByClassName("gallery")[0];
-  for (var i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i++) {
     photosConteiner.appendChild(createImgElement(i));
   }
   dotsConteiner = document.createElement("div");
@@ -24,13 +24,13 @@ function addAllPhotosToHtml(amount) {
 
 function addAllDotsToHtml(amount) {
   dotsConteiner = document.getElementsByClassName("dot_control")[0];
-  for (var i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i++) {
     dotsConteiner.appendChild(createDotElement(i));
   }
 }
 
 function createImgElement(i) {
-  var imgTag = document.createElement("img");
+  let imgTag = document.createElement("img");
   imgTag.id = "photo_carusel_dot" + i;
   imgTag.src = "media/imgGal (" + i + ").png";
   imgTag.alt = "Photo for gallery";
@@ -39,9 +39,9 @@ function createImgElement(i) {
 }
 
 function createDotElement(i) {
-  var divTag = document.createElement("div");
+  let divTag = document.createElement("div");
   divTag.classList.add("dot");
-  var dotImgTag = document.createElement("img");
+  let dotImgTag = document.createElement("img");
   dotImgTag.id = "dot" + i;
   dotImgTag.src = "media/dot_gallery.png";
   dotImgTag.alt = "dot";
@@ -50,17 +50,17 @@ function createDotElement(i) {
 }
 
 function addDotEventListener(amount) {
-  for (var i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i++) {
     document.getElementById("dot" + i).addEventListener("click", showImg);
   }
 }
 
 function showImg() {
-  for (var i = 0; i < amountOfPhotos; i++) {
+  for (let i = 0; i < amountOfPhotos; i++) {
     document.getElementById("photo_carusel_dot" + i).classList.remove("show");
   }
   document.getElementById("photo_carusel_" + this.id).classList.add("show");
-  for (var i = 0; i < amountOfPhotos; i++) {
+  for (let i = 0; i < amountOfPhotos; i++) {
     document.getElementById("dot" + i).classList.remove("current");
   }
   this.classList.add("current");
@@ -70,12 +70,12 @@ function showImg() {
 function showImgSlideshow() {
   if (currentPhoto < amountOfPhotos - 1) {
     document;
-    for (var i = 0; i < amountOfPhotos; i++) {
+    for (let i = 0; i < amountOfPhotos; i++) {
       document
         .getElementById("photo_carusel_dot" + currentPhoto)
         .classList.remove("show");
     }
-    for (var i = 0; i < amountOfPhotos; i++) {
+    for (let i = 0; i < amountOfPhotos; i++) {
       document.getElementById("dot" + currentPhoto).classList.remove("current");
     }
     currentPhoto++;
@@ -84,12 +84,12 @@ function showImgSlideshow() {
       .classList.add("show");
     document.getElementById("dot" + currentPhoto).classList.add("current");
   } else {
-    for (var i = 0; i < amountOfPhotos; i++) {
+    for (let i = 0; i < amountOfPhotos; i++) {
       document
         .getElementById("photo_carusel_dot" + currentPhoto)
         .classList.remove("show");
     }
-    for (var i = 0; i < amountOfPhotos; i++) {
+    for (let i = 0; i < amountOfPhotos; i++) {
       document.getElementById("dot" + currentPhoto).classList.remove("current");
     }
     document.getElementById("photo_carusel_dot0").classList.add("show");
